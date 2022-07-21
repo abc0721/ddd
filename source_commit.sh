1ce0ddb52d21b0cf01a634728d724fc98b9efd19
liushuang25 <liushuang25@jd.com>
2022-07-21 20:05:27 +0800

    commit

---
+++ source_commit.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+git init
+git add -A
+git commit -m 'commit'
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:abc0721/learn-book.git master:main
+
+cd -
